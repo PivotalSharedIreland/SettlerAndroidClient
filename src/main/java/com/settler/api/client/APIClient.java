f317a73ca4ed9040e395adb770c998c7af29950c
Bengt Hammarlund and Fabio Mangione <pair+bengt+fabio@pivotallabs.com>
2016-02-02 15:14:46 +0000

    Removed iBinder interface from APICLient

--- src/main/java/com/settler/api/client/APIClient.java
+++ src/main/java/com/settler/api/client/APIClient.java
@@ -1,14 +1,12 @@
 package com.settler.api.client;
 
 
-import android.os.IBinder;
-
 import com.settler.api.Property;
 
 import java.io.IOException;
 import java.util.List;
 
-public interface APIClient extends IBinder {
+public interface APIClient {
 
     List<Property> listProperties() throws IOException;
 
